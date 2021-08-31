2b4c14b402d103c0710f4e79a5e1cc2846fa9ab7
sachiniSam <sachiniprof@gmail.com>
2021-08-31 14:33:03 +0530

    Update expression suggestions

--- src/index.tsx
+++ src/index.tsx
@@ -8,16 +8,31 @@ interface ExpressionType {
     supportedExprs: ExpressionType[]
 }
 
+const conditionalExpr : ExpressionType = {
+    kind: "ConditionalExpression",
+    template: ["expression", "?", "expression",":", "expression"], // conditional expr has two templates
+    supportedExprs: [] // suggestions, in Ui we show the vars and functions
+}
+// enum ComparisonOperatorTypes {
+//     GREATER_THAN = ">",
+//     LESS_THAN = "<",
+//     EQUALITY = "==",
+//     NOT_EQUAL = "!=",
+//   }
+// const comparisonOperators = {
+//     operators : [">","<",]
+// }
+
 const comparisonExpr : ExpressionType = {
     kind: "comparisonExpression",
-    template: ["expression","operator","expression"],
-    supportedExprs: []
+    template: ["expression","operator","expression"], // how can we manage the operators
+    supportedExprs: [conditionalExpr]
 }
 
 const logicalExpr : ExpressionType = {
     kind: "logicalExpression",
     template: ["expression","operator","expression"],
-    supportedExprs: [comparisonExpr]
+    supportedExprs: [comparisonExpr],
 }
 
 const ifElseExprType : ExpressionType = {
@@ -26,11 +41,16 @@ const ifElseExprType : ExpressionType = {
     supportedExprs: [comparisonExpr,logicalExpr]
 }
 
+
+
 function TreeEditor (props:{exprItem: ExpressionType, onClick:(tree:ExpressionType) => void} ){
     const {exprItem, onClick} =props;
     return (
         <ul>
-            <h5>expression list </h5>
+            <h5>{exprItem.template.map(templateItem => (
+                <button className="btnTemplate">{templateItem}</button>
+            ))}</h5>
+            <h5>expression list: Parent : {exprItem.kind} </h5>
             {console.log(exprItem.kind)}
             { exprItem.supportedExprs.map(expression => (       
                  <button className="button1 App-expression-component" onClick = {() => onClick(expression)}>{expression.kind}</button>
@@ -46,7 +66,7 @@ function App() {
     return (
  
         <div className="App">
-            <header className="App-header">Expression Suggestions</header>
+            <header className="App-header">Expression Editor base</header>
             <TreeEditor exprItem={selectedExpressionType} onClick = {setSelectedExpressionType} />
         </div>
  
