a59c61cee822068a7403b97fe6314fd5da735516
sachiniSam <sachiniprof@gmail.com>
2021-08-31 12:51:23 +0530

    Add initial structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+interface ExpressionType {
+    kind: string,
+    parent ?: ExpressionType,
+    template: string[]
+    supportedExprs: ExpressionType[]
+}
+
+const comparisonExpr : ExpressionType = {
+    kind: "comparisonExpression",
+    template: ["expression","operator","expression"],
+    supportedExprs: []
+}
+
+const logicalExpr : ExpressionType = {
+    kind: "logicalExpression",
+    template: ["expression","operator","expression"],
+    supportedExprs: [comparisonExpr]
+}
+
+const ifElseExprType : ExpressionType = {
+    kind: "ifElseExpression",
+    template: ["expression"],
+    supportedExprs: [comparisonExpr,logicalExpr]
+}
+
+function TreeEditor (props:{exprItem: ExpressionType, onClick:(tree:ExpressionType) => void} ){
+    const {exprItem, onClick} =props;
+    return (
+        <ul>
+            <h5>expression list </h5>
+            {console.log(exprItem.kind)}
+            { exprItem.supportedExprs.map(expression => (       
+                 <button className="button1 App-expression-component" onClick = {() => onClick(expression)}>{expression.kind}</button>
+            ))}
+            
+        </ul>
+    );
+}
+
+
+function App() {
+    const [selectedExpressionType, setSelectedExpressionType] = React.useState(ifElseExprType)
+    return (
+ 
+        <div className="App">
+            <header className="App-header">Expression Suggestions</header>
+            <TreeEditor exprItem={selectedExpressionType} onClick = {setSelectedExpressionType} />
+        </div>
+ 
+    );
+ 
+ }
+
+ ReactDOM.render(
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
